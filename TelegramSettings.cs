b7b3c4b5f145057a88ba36dccae45cf2c79e2a26
Vitalii Yelizarov <v196093@gmail.com>
2024-05-01 00:16:04 +0200

    Implemented storing Telegram settings in TelegramSettings.json file and deserealising it. Message about Termin status would be sent only in case when there's will be available Termin

---
+++ TelegramSettings.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using SiteClicker_Parser.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SiteClicker_Parser.SettingsStorage;
+
+namespace SiteClicker_Parser
+{
+    internal class TelegramSettings
+    {
+        public TelegramSettings(string pathToSettingsFile) 
+        {
+            if (File.Exists(pathToSettingsFile))
+            {
+                string json = File.ReadAllText(pathToSettingsFile);
+                var tempSettings = JsonConvert.DeserializeObject<TelegramSettings>(json);
+
+                TOKEN = API_Token = tempSettings.API_Token;
+                CHAT_ID = ChatId = tempSettings.ChatId;
+
+            }
+            else
+            {
+                TOKEN = API_Token = string.Empty;
+                CHAT_ID = ChatId = -1;
+            }
+        }
+        public string API_Token { get; private set; }
+        public long ChatId { get; private set; }
+    }
+}
