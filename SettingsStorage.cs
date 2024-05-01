78f375b8c6441e398ff799fd95f658a01d4d0883
Vitalii Yelizarov <v196093@gmail.com>
2024-05-01 22:13:27 +0200

    Small refactorings (changed access level to some predefined settings)

--- SettingsStorage.cs
+++ SettingsStorage.cs
@@ -6,28 +6,29 @@ namespace SiteClicker_Parser
 {
     public class SettingsStorage
     {
-        public static bool IsDebug = false;
-        public static bool IsException = false;
-        public static bool IsRunning = false;
-        public static bool ImmediateStart = false;
-        public static int DELAY = 2000;
-        public static int MAX_DELAY = 5000;
-        public static int REQUEST_REPEAT_TIME = 0;
-        public static readonly string DEFAULT_REQUEST_REPEAT_TIME_STRING = "3";
-        public static readonly int DEFAULT_REQUEST_REPEAT_TIME = 3 * 60 * 1000; //3 minutes
-        public static readonly long LOGFILE_MAXSIZE_BYTES = 50 * 1024 * 1024; //50 Mbytes
-        public static int _IterationNumber = 1;
+        public const int DELAY = 2000;
+        public const int MAX_DELAY = 5000;
+        public const string DEFAULT_REQUEST_REPEAT_TIME_STRING = "3";
+        public const string CSS_SELECTOR = "input.btn.btn-primary.onehundred.pull-right";
+        public const string CLASS_NAME = "h1like";
+        public const int DEFAULT_REQUEST_REPEAT_TIME = 3 * 60 * 1000; //3 minutes
+        public const long LOGFILE_MAXSIZE_BYTES = 50 * 1024 * 1024; //50 Mbytes
+        public const string WEB_ADDRESS = "https://termine.staedteregion-aachen.de/auslaenderamt/";
 
-        public static string WEB_ADDRESS = "https://termine.staedteregion-aachen.de/auslaenderamt/";
         public static readonly string APP_PATH = Assembly.GetExecutingAssembly().Location;
         public static readonly string APP_FOLDER_PATH = APP_PATH.Substring(0, APP_PATH.LastIndexOf('\\') + 1);
         public static readonly string LOG_PATH = APP_PATH + ".log";
         public static readonly string TG_SETTINGS_PATH = APP_FOLDER_PATH + "TelegramSettings.json";
-        public static readonly string CSS_SELECTOR = "input.btn.btn-primary.onehundred.pull-right";
-        public static readonly string CLASS_NAME = "h1like";
+        public static readonly string LOCK_FILE_PATH = APP_FOLDER_PATH + "base.dll";
+
+        public static bool IsDebug = false;
+        public static bool IsException = false;
+        public static bool IsRunning = false;
+        public static bool ImmediateStart = false;
+        public static int REQUEST_REPEAT_TIME = 0;
+        public static int _IterationNumber = 1;
 
         public static FileStream lockFile = null;
-        public static readonly string LOCK_FILE_PATH = APP_FOLDER_PATH + "base.dll";
 
         public static IReadOnlyList<string> IdsList = new List<string>
         {
